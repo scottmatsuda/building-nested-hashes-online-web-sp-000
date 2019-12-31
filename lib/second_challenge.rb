6352855e0a2a9c63332bed973afa84c81c0c14bf
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-31 21:08:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,11 +4,12 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends
+      :hero_friends = []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :heroine_friends = []
       }}
-
-  
 end
